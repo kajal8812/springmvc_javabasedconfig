eb8dad471f05263204aae7ab3b7550cf6ac14008
DESKTOP-V3O34Q3\Srinivas <kajalkumari8812@gmail.com>
2023-07-25 17:43:38 +0530

    initial commit

---
+++ src/main/java/com/learning/MvcConfig.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan({ "com.learning" })
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+}
